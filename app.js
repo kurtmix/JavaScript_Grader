8594a7cea1a76440d8ecb8deebfdfccd955b7136
kurtmix <kurtmix123@gmail.com>
2016-08-03 11:43:56 -0400

    Transfered Grading App from C9

---
+++ app.js
@@ -0,0 +1,17 @@
+// Grading App
+function giveGrade() {
+  var grade = document.getElementById("grade").value;
+
+  if (grade>=100 && grade>=90){
+   alert("A");}
+ else if (grade<=89 && grade>=80){
+   alert("B");}
+ else if (grade<=79 && grade>=70){
+   alert("C");}
+ else if (grade<=69 && grade>=65){
+   alert("D");}
+ else if (grade<=64){
+   alert("F");}   
+//Input your code below:
+//
+  }
